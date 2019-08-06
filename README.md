40b6ff28517478f342d768e91421ff9b7a8a7779
Jeyko <jakeactually@outlook.com>
2019-08-06 11:26:48 -0500

    much better interface, fixed thumbs

--- README.md
+++ README.md
@@ -10,34 +10,6 @@ You must have [rust](https://www.rust-lang.org/es-ES/install.html) installed. Th
 cargo run
 ```
 
-And by default it will run Pong. You can change the game in src/main.rs
-
-```rust
-if args.len() < 2 {
-    run::run("roms/Pong (1 player).ch8", false, false);
-    return
-}
-```
-
-with any of the availables in the roms folder.
-
-The roms/roms.json file has data about each game. If it were to have "quirks", they should be set in the "run" function.
-
-```json
-{
-    "title": "BLINKY",
-    "file": "Blinky [Hans Christian Egeberg, 1991].ch8",
-    "quirks": { "loadStore": true, "shift": true },
-    "description": "Blinky (1991), by Hans Christian Egeberg<br/><br/>Pacman clone.<br/>3, 6 - down/up. 7, 8 - left/right"
-}
-```
-
-```rust
-pub fn run(rom: &str, shift_hack: bool, memory_hack: bool)
-```
-
-Most games run with "false, false" though.
-
 # Screenshots
 
 Blinky
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate piston_window;
 extern crate rand;
+extern crate serde;
+extern crate serde_json;
 
 mod assembler;
 mod chip8;
@@ -10,21 +12,73 @@ mod hardware;
 mod run;
 
 use assembler::assemble;
+use serde::{Deserialize, Serialize};
 use std::env;
-use std::fs::File;
-use std::io::{Read, Write};
+use std::fs::{File};
+use std::io::{Read, Write, stdin};
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Game {
+    title: String,
+    file: String,
+    quirks: Option<Quirks>,
+    description: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Quirks {
+    load_store: Option<bool>,
+    shift: Option<bool>
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        run::run("roms/Pong (1 player).ch8", true, true);
-        return
+        play();
     }
 
     if args[1] == "asm" {
         let mut text = String::new();
         let _ = File::open("dev/rom.asm").unwrap().read_to_string(&mut text);
         let _ = File::create("dev/rom.ch8").unwrap().write_all(assemble(text).as_slice());
+    }
+}
+
+fn play() {
+    let file = File::open("roms/roms.json").unwrap();
+    let games: Vec<Game> = serde_json::from_reader(file).unwrap();
+
+    for (i, game) in games.iter().enumerate() {
+        println!("{} {}", i, game.title)
+    }
+
+    loop {
+        ask(&games);
+    }
+}
+
+fn ask(games: &Vec<Game>) {
+    println!();
+    println!("choose a game: ");
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    let index: u32;
+    if let Ok(number) = input.trim().parse::<u32>() {
+        index = number;
+    } else {
+        println!("not a number");
+        return
+    }
+
+    if let Some(game) = games.iter().nth(index as usize) {
+        let rom = format!("roms/{}", game.file);
+        let quirks = game.clone().quirks;
+        let shift = quirks.clone().and_then(|q| q.shift).unwrap_or(false);
+        let load_store = quirks.and_then(|q| q.load_store).unwrap_or(false);
+        run::run(rom.as_str(), shift, load_store);
+    } else {
+        println!("not a game");
     }
 }
